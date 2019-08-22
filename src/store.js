8e5ecc44744e2ae56e84e43dac169942b2accf3d
mr.scrpt <mr.scrpt@gmail.com>
2019-08-22 17:33:07 +0300

    edists applyMiddleware

--- src/store.js
+++ src/store.js
@@ -10,8 +10,7 @@ const store = createStore(
   rootReducer,
   INITIAL_STATE,
   compose(
-    applyMiddleware(searchMiddleware),
-    applyMiddleware(showMiddleware),
+    applyMiddleware(searchMiddleware, showMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
   );
 
